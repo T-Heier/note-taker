d9343843aabcc742aafb68c3265b997985cfb15c
T-Heier <trentheier@gmail.com>
2021-04-24 20:08:38 -0400

    finished the routing to get and post

--- db/notes.js
+++ db/notes.js
@@ -1,5 +1,39 @@
+const { json } = require('express');
 const fs = require('fs');
+const util = require('util')
+const asyncReadFile = util.promisify(fs.readFile)
+const asyncWriteFile = util.promisify(fs.writeFile)
 
 class Notes {
+    getNotesDB() {
+        console.log('connected get')
+        return asyncReadFile("db/db.json").then(notes => {
 
+            return JSON.parse(notes)
+        })
+    }
+
+    writeNewNote(note) {
+        const newNote = {...note, id: Math.random().toString()}
+        // notes.push(newNote)
+        return this.getNotesDB()
+        .then(notes => [...notes, newNote])
+        .then(newNotes => {
+            return asyncWriteFile("db/db.json", JSON.stringify(newNotes))
+        }).then( () => newNote)
+
+    }
+    deleteNote(id) {
+        console.log(id)
+        return this.getNotesDB()
+        .then(notes => {
+            console.log(notes)
+            return notes.filter(note => note.id !== id)})
+        .then(updatedNotes => {
+            return asyncWriteFile("db/db.json", JSON.stringify(updatedNotes))
+        });
+
+    }
 }
+
+module.exports = new Notes()
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,27 @@
+const notedb = require('../db/notes')
 module.exports = (app) => {
     app.get("/api/notes", (req, res) => {
-        res.sendFile(path.join(__dirname, "../db/db.json"))
-    });
+        notedb.getNotesDB()
+        .then(notes => {
+            return res.json(notes)
+        })
+    })
+
+    app.post("/api/notes", (req, res) => {
+        console.log("connected post")
+        notedb.writeNewNote(req.body)
+        .then(note => {
+            return res.json(note)
+        })
+    })
+
+    app.delete(`/api/notes/:id`, (req, res) => {
+        console.log('delete note')
+        notedb.deleteNote(req.params.id)
+        .then(() => {
+            res.json({okay: true})
+        })
+    })
+
 }
+
